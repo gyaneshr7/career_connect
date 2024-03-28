b09eaacba583c296097e8d770b6381838d3853ee
GYANESH <gyaneshr7@gmail.com>
2024-03-28 19:34:13 +0530

    commit initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/home/Home";
+import Settings from "./pages/settings/Settings";
+import Write from "./pages/write/Write";
+import TopBar from "./components/topBar/TopBar.jsx";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Post from "./components/post/Post";
+import Single from "./pages/single/Single";
+import { useContext } from "react";
+import { Context } from "./context/Context";
+function App() {  
+  const {user}=useContext(Context)
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<BrowserRouter>
+     <TopBar/>
+<Routes>
+  <Route path="/"  exact element={ <Home/>} ></Route>
+  <Route path="/register"  exact element={user?<Home/> :<Register/>} ></Route>
+  <Route path="/login"  exact element={user ? <Home/> :<Login/> } ></Route>
+  <Route path="/write"  exact element={user ? <Write/>:<Register/>} ></Route>
+  <Route path="/settings"  exact element={user ? <Settings/>: <Register/>} ></Route>
+  <Route path="/post/:postId"  exact element={<Single></Single>} ></Route>
+  
+</Routes>
+</BrowserRouter>
   );
 }
 
---
+++ src/components/topBar/TopBar.jsx
@@ -0,0 +1,68 @@
+import React, { useContext } from 'react'
+import './topbar.css'
+import { Link } from 'react-router-dom'
+import { Context } from '../../context/Context'
+function TopBar() {  
+  const {user,dispatch}=useContext(Context);  
+  const handleLogout =()=>{
+        dispatch({type:"LOGOUT"})
+  } 
+
+  const PF="http://localhost:7000/images/"
+  return (
+    <div className='top' > 
+     <div className="topLeft">
+     <a href="https://www.instagram.com/yogesh_whetwar/" target='_blank'><i className="topIcon fa-brands fa-square-instagram">  </i></a>
+    <a href="https://www.facebook.com/yogesh.whetwar/"> <i className="topIcon fa-brands fa-square-facebook"></i></a>
+    <a href="https://www.linkedin.com/in/yogesh-whetwar-303b43203/"><i className="topIcon fa-brands fa-linkedin"></i></a> 
+    <a href="https://github.com/Yogesh-Whetwar"><i className="topIcon fa-brands fa-square-github"></i></a> 
+     </div>
+     <div className="topCenter">
+        <ul  className='topList' >
+         <Link to='/' className='link' >
+          <li className='topListItem'  >HOME</li>
+          </Link>   
+         <Link to='/'className='link'>
+            <li className='topListItem' >ABOUT</li>
+
+          </Link>   
+         <Link to='/'className='link'>
+
+            <li className='topListItem' >CONTACT</li>
+          </Link>   
+         <Link to='/write'className='link'>
+            <li className='topListItem' >WRITE</li>
+          </Link>   
+         <Link to='/'className='link'>
+            <li className='topListItem'onClick={handleLogout} > {user && "LOGOUT"}</li>
+          </Link>   
+        </ul>
+     </div>
+     <div className="topRight">
+      <Link to='/settings' className='link' >
+        {
+          user ? (
+            <img   
+            className='topImg'
+            src={PF+user.profilePic} alt="" /> 
+          ) : (  
+              <ul className='topList' >
+ <li className='topListItem tp'> <Link className='link' to='/login'>LOGIN</Link></li>
+ <li className='topListItem tp'>  
+ <Link className='link' to='/register'>REGISTER</Link>
+ </li>
+              </ul>
+              
+              
+            
+          )
+        }
+       
+          <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </Link>
+     </div>
+    </div>
+  )
+}
+
+export default TopBar
